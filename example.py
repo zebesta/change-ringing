1e0a426884e2db1b4c35d4fa10b951237bfc1df6
zebesta <chris.sebesta@gmail.com>
2016-11-15 16:26:26 -0500

    added pages and error checking for form for proper flow between change string and song

---
+++ example.py
@@ -0,0 +1,6 @@
+class Example:
+    def __init__(self, origAudioLink, origSpectroLink, dreamedAudioLink, dreamedSpectroLink):
+        self.origAudioLink = origAudioLink
+        self.origSpectroLink = origSpectroLink
+        self.dreamedAudioLink = dreamedAudioLink
+        self.dreamedSpectroLink  = dreamedSpectroLink
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,13 +6,25 @@ def home():
     songs = ["No Diggity", "Hypnotize", "99 Problems", "Party in the USA"]
     return render_template('form.html', songs=songs)
 
-@app.route("/accept")
+@app.route("/accept", methods=['POST', 'GET'])
 def accept():
-    return "You submitted the form!!!"
+    if request.method == 'POST':
+        print("HALP!!")
+        if 'change' not in request.form:
+            return "No change in the input!"
+        change = str(request.form['change'])
+        if len(change) < 1:
+            song = str(request.form['song'])
+            return "No string selected, looking at song: " + song
+
+        return "You submitted the form!!! With change: " + change
+    if request.method == 'GET':
+        return "A get request to accept?!?"
+
 
 @app.route("/examples")
 def examples():
     return render_template('examples.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',port=8000)
