8fbccc6f1d64dbdf0a942ae0667a2e7934abb1e0
zebesta <chris.sebesta@gmail.com>
2016-11-15 16:30:16 -0500

    added heroku web name for form

--- example.py
+++ example.py
@@ -1,6 +1,4 @@
 class Example:
-    def __init__(self, origAudioLink, origSpectroLink, dreamedAudioLink, dreamedSpectroLink):
-        self.origAudioLink = origAudioLink
-        self.origSpectroLink = origSpectroLink
-        self.dreamedAudioLink = dreamedAudioLink
-        self.dreamedSpectroLink  = dreamedSpectroLink
+    def __init__(self, audio, image):
+        self.audio = audio
+        self.image = image
