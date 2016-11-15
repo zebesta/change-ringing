8f24d87251902d869b8c7cbc19fd7e0a99fe3beb
zebesta <chris.sebesta@gmail.com>
2016-11-15 15:40:55 -0500

    adding pages and a form

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    songs = ["No Diggity", "Hypnotize", "99 Problems", "Party in the USA"]
+    return render_template('form.html', songs=songs)
+
+@app.route("/accept")
+def accept():
+    return "You submitted the form!!!"
+
+@app.route("/examples")
+def examples():
+    return render_template('examples.html')
 
 if __name__ == "__main__":
     app.run()
